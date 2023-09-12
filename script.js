f6783fdde8c8f401f4d5048d8546e99df8b72202
GerrickB <27402037+GerrickB@users.noreply.github.com>
2023-09-11 18:05:54 -0700

    Update addToLibrary and displayBook functions

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 
 // the constructor
-function Book(title, author, pages, read) {
+function Book(title, author, pages, read, id) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
+  this.id = id;
+  // unused
   this.info = function() {
     return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
   }
 }
 
 //do stuff
-function addBookToLibrary(title, author, pages, read) {
-  myLibrary.push(new Book(title, author, pages, read));
+function addBookToLibrary(title, author, pages, read, id) {
+  myLibrary.push(new Book(title, author, pages, read, id));
 }
 
 // refresh library so no duplication
@@ -27,33 +29,41 @@ function displayBook() {
   // refresh library so no duplication
   refreshBook();
   for (let i = 0; i < myLibrary.length; i++) {
-    const container = document.querySelector('.container');
-    const div = document.createElement('div');
-    div.classList.add('books')
-    const h2 = document.createElement('h2');
-    const h3 = document.createElement('h3');
     let book = myLibrary[i];
-    h2.textContent = book.title;
-    h3.textContent = book.author;
-    div.appendChild(h2);
-    div.appendChild(h3);
-    container.appendChild(div);
+    const container = document.querySelector('.container');
+
+    const div = document.createElement('div');
+    div.classList.add('books');
+  
+    const pre = document.createElement('pre');
+    pre.textContent = `Title: ${book.title}\nAuthor: ${book.author}\nLength: ${book.pages} pages\nRead Status: ${book.read}`
+
+    const p = document.createElement('p');
+    p.textContent = `book id: ${book.id}`
+
+    // img button to delete book
+    const img = document.createElement('img');
+    img.src = 'icons/delete_FILL0_wght400_GRAD0_opsz24.svg'
+    img.addEventListener('click', () => {
+      // dynamic index choosing
+      let correctIndex;
+      for (let i = 0; i < myLibrary.length; i++) {
+        if (myLibrary[i].id === book.id)  {
+          correctIndex = i;
+        }
+      }
+      myLibrary.splice(correctIndex, 1);
+      displayBook();
+    });
+
+    div.appendChild(pre);
+    div.appendChild(p);
+    div.appendChild(img);
+    container.appendChild(div); 
   }
 }
 
-function removeBook() {
-  //maybe use index
-}
-
-const myLibrary = [];
-myLibrary.push(new Book('The Hobbit', 'J.R.R. Tolkien', 295, false));
-
-const btn = document.querySelector('#btn');
-btn.addEventListener('click', displayBook)
-
-/* ----------------------------------------*/
 /* ------------Dialog Form----------------*/
-/* ----------------------------------------*/
 const showDialog = document.getElementById("showDialog");
 const myDialog = document.getElementById("myDialog");
 const titleInput = myDialog.querySelector("#titleInput");
@@ -61,10 +71,7 @@ const authorInput = myDialog.querySelector("#authorInput");
 const pageNumbers = myDialog.querySelector("#pageNumbers")
 const trueRead = myDialog.querySelector("#trueRead");
 const falseRead = myDialog.querySelector("#falseRead");
-// output test
-const outputBox = document.querySelector("output");
 const confirmBtn = myDialog.querySelector("#confirmBtn");
-
 
 showDialog.addEventListener("click", () => {
   myDialog.showModal();
@@ -78,17 +85,21 @@ myDialog.addEventListener("close", (e) => {
     readValue = false;
   }
 
-  addBookToLibrary(titleInput.value, authorInput.value, pageNumbers.value, readValue)
-  
-  // const returnValue = `Title: ${titleInput.value}, Author: ${authorInput.value}, pages: ${pageNumbers.value}, Have I read it? ${readValue}`
-  // outputBox.value = returnValue;
+  addBookToLibrary(titleInput.value, authorInput.value, pageNumbers.value, readValue, bookID);
+  bookID++;
+  displayBook();
 });
 
 confirmBtn.addEventListener("click", (e) => {
   e.preventDefault();
   myDialog.close();
 });
+/* ----------------------------------------*/
 
-// // testing
-// //console.log(book1.info());
-// //console.log(Book.prototype);
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', displayBook);
+
+const myLibrary = [];
+let bookID = 1;
+// add first book manually /explicitly?
+myLibrary.push(new Book('The Hobbit', 'J.R.R. Tolkien', 295, false, 0));
